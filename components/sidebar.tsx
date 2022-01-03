9fc015d890ee080b45dac81e8995f97407ace508
Olivia Cameron <olivia.cameronj15@gmail.com>
2022-01-03 16:51:23 -0500

    feat: playlist

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -20,6 +20,8 @@ import {
   MdFavorite,
 } from "react-icons/md";
 
+import { usePlaylist } from "../lib/hooks";
+
 const navMenu = [
   {
     name: "Home",
@@ -51,9 +53,11 @@ const musicMenu = [
   },
 ];
 
-const playlists = new Array(30).fill(1).map((_, i) => `Playlist ${i + 1}`);
+// const playlists = new Array(30).fill(1).map((_, i) => `Playlist ${i + 1}`);
 
 const Sidebar = () => {
+  const { playlists } = usePlaylist();
+
   return (
     <Box
       width="100%"
@@ -110,10 +114,10 @@ const Sidebar = () => {
         <Box height="66%" overflowY="auto" paddingY="20px">
           <List spacing={2}>
             {playlists.map((playlist) => (
-              <ListItem paddingX="20px" key={playlist}>
+              <ListItem paddingX="20px" key={playlist.id}>
                 <LinkBox>
                   <NextLink href="/" passHref>
-                    <LinkOverlay>{playlist}</LinkOverlay>
+                    <LinkOverlay>{playlist.name}</LinkOverlay>
                   </NextLink>
                 </LinkBox>
               </ListItem>
