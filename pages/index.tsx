c0e1c9ac689f47dc8872892afa6069746ef76be8
Olivia Cameron <olivia.cameronj15@gmail.com>
2022-01-05 10:23:04 -0500

    feat: homepage user data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,28 @@
 import { Box, Flex, Text } from "@chakra-ui/layout";
-import { Image } from "@chakra-ui/react";
+import { Image, Skeleton } from "@chakra-ui/react";
 import GradientLayout from "../components/gradientLayout";
 import prisma from "../lib/prisma";
 import { artistsData } from "../prisma/songsData";
+import { useMe } from "../lib/hooks";
 
 const Home = () => {
+  const { user, isLoading } = useMe();
+
+  const dataLoading = () => {
+    if (isLoading) {
+      return <Skeleton height="25px" width="255px" marginBottom="15px"/>;
+    }
+  };
+
   return (
     <GradientLayout
       roundImage
       color="gray"
       subtitle="profile"
-      title="Olivia Cameron"
-      description="10 public playlists"
+      title={isLoading ? dataLoading() : `${user?.firstName} ${user?.lastName}`}
+      description={
+        isLoading ? dataLoading() : `${user?.playlistsCount} public playlists`
+      }
       image="https://tinted-gym-f99.notion.site/image/https%3A%2F%2Fdl.dropboxusercontent.com%2Fs%2Fbgiv0ssz3xpotz9%2Fpeep.png%3Fdl%3D0?table=block&id=33f9771b-0e6f-4a72-832c-69ed2d41f290&spaceId=511cd811-5561-4a61-b550-c4086b4afafb&width=2000&userId=&cache=v2"
     >
       <Box color="white" paddingX="40px">
