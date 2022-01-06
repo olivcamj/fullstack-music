85caf375e164efb64d82c71ea560e6a1b5795d97
Olivia Cameron <olivia.cameronj15@gmail.com>
2022-01-06 11:09:01 -0500

    feat: gradient playlist page

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,11 +1,41 @@
+import GradientLayout from "../../components/gradientLayout";
 import { validateToken } from "../../lib/auth";
 import prisma from "../../lib/prisma";
 
-const Playlist = ({ playlist }) => {
-  return <div>{playlist.name}</div>;
+// return a random color
+const getBGColor = (id) => {
+  const colors = [
+    "red",
+    "green",
+    "blue",
+    "teal",
+    "yellow",
+    "orange",
+    "purple",
+    "gray",
+  ];
+
+  return colors[id - 1] || colors[Math.floor(Math.random() * colors.length)];
 };
 
-export const getServerSideProps =  async ({ query, req }) => {
+const Playlist = ({ playlist }) => {
+  const color = getBGColor(playlist.id);
+
+  return (
+    <GradientLayout
+      color={color}
+      roundImage={false}
+      title={playlist.name}
+      subtitle="playlist"
+      description={`${playlist.songs.length} songs`}
+      image={`https://picsum.photos/400?random=${playlist.id}`}
+    >
+      <div>cool</div>
+    </GradientLayout>
+  );
+};
+
+export const getServerSideProps = async ({ query, req }) => {
   const { id } = validateToken(req.cookies.TRAX_ACCESS_TOKEN);
   const [playlist] = await prisma.playlist.findMany({
     where: {
